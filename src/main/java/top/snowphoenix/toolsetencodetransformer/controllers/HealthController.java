efe1104406e892c522b3b676c302eedff4fabd1f
SnowPhoenix0105 <1595763343@qq.com>
2021-09-29 00:58:51 +0800

    fix a bug lead to wrong package name

--- src/main/java/top/snowphoenix/toolsetencodetransformer/controllers/HealthController.java
+++ src/main/java/top/snowphoenix/toolsetencodetransformer/controllers/HealthController.java
@@ -1,4 +1,4 @@
-package top.snowphonix.toolsetusercenter.controllers;
+package top.snowphonix.toolsetencodetransformer.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
